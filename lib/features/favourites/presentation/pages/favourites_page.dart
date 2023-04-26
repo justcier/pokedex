0bc5557382dd75740a1b6019098b62326439311b
justcier <syrina02@gmail.com>
2023-04-26 15:33:27 +0200

    Displayed favourites pokemon on FavouritesPage

--- lib/features/favourites/presentation/pages/favourites_page.dart
+++ lib/features/favourites/presentation/pages/favourites_page.dart
@@ -1,7 +1,11 @@
 import 'package:auto_route/auto_route.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:pokedex_rest/common/widgets/common_scaffold.dart';
 import 'package:pokedex_rest/core/strings/strings.dart';
+import 'package:pokedex_rest/features/favourites/presentation/cubits/favourites_cubit.dart';
+import 'package:pokedex_rest/features/favourites/presentation/cubits/favourites_state.dart';
+import 'package:pokedex_rest/features/pokemon_list/presentation/widgets/pokemon_list_widget.dart';
 import 'package:pokedex_rest/style/color_tokens.dart';
 import 'package:pokedex_rest/style/text_style_tokens.dart';
 
@@ -19,7 +23,19 @@ class FavouritesPage extends StatelessWidget {
         ),
         backgroundColor: ColorTokens.secondaryColor,
       ),
-      body: Container(),
+      body: BlocBuilder<FavouritesCubit, FavouritesState>(
+        builder: (context, state) {
+          print(state.favouritesList);
+          return PokemonListWidget(
+            pokemonDetailsList: state.favouritesList,
+            gridViewScrollController: ScrollController(),
+            isLoading: false,
+            onDoubleTap: (pokemonDetails) => context
+                .read<FavouritesCubit>()
+                .toggleFavouriteState(pokemonDetails),
+          );
+        },
+      ),
     );
   }
 }
