6cd90c523a701af391895e09cf98af9d809f559e
justcier <syrina02@gmail.com>
2023-05-23 23:22:37 +0200

    Added TextField to SearchWidget

--- lib/features/search/presentation/widgets/search_widget.dart
+++ lib/features/search/presentation/widgets/search_widget.dart
@@ -1,16 +1,36 @@
 import 'package:flutter/material.dart';
 import 'package:pokedex_rest/core/extensions/build_context_extensions.dart';
+import 'package:pokedex_rest/core/strings/strings.dart';
 import 'package:pokedex_rest/style/color_tokens.dart';
 import 'package:pokedex_rest/style/dimensions.dart';
 import 'package:pokedex_rest/style/text_style_tokens.dart';
 
-class SearchWidget extends StatelessWidget {
+class SearchWidget extends StatefulWidget {
   const SearchWidget({Key? key}) : super(key: key);
 
   @override
+  State<SearchWidget> createState() => _SearchWidgetState();
+}
+
+class _SearchWidgetState extends State<SearchWidget> {
+  @override
   Widget build(BuildContext context) {
+    final TextEditingController controller = TextEditingController();
+
+    @override
+    void initState() {
+      super.initState();
+      controller;
+    }
+
+    @override
+    void dispose() {
+      controller.dispose();
+      super.dispose();
+    }
+
     return Container(
-      height: (context.screenHeight / 30),
+      height: (context.screenHeight / 35),
       decoration: BoxDecoration(
         color: ColorTokens.white,
         borderRadius: BorderRadius.circular(Dimensions.sizeL),
@@ -25,9 +45,13 @@ class SearchWidget extends StatelessWidget {
           mainAxisAlignment: MainAxisAlignment.spaceBetween,
           children: [
             Flexible(
-              child: Text(
-                'Choose your Pokemon!',
+              child: TextField(
+                controller: controller,
                 style: TextStyleTokens.mainDescription.copyWith(fontSize: 6),
+                decoration: const InputDecoration(
+                  border: UnderlineInputBorder(),
+                  hintText: Strings.searchHint,
+                ),
               ),
             ),
             const Icon(
